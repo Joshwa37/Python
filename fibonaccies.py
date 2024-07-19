2916ef096bfe732b73e4a8af0e0894ba5ab278f5
Joshwa37 <146755994+Joshwa37@users.noreply.github.com>
2024-07-19 18:22:59 +0530

    Add files via upload

---
+++ fibonaccies.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def fibo(n):
+    a=0
+    for i in range(1,n):
+        a=a+i
+        print(a)
+fibo(10)        
+        
+
+
+# In[ ]:
+
+
+
+
