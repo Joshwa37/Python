c762ac675ec8726a291be17b07ead40efacf34ab
Joshwa37 <146755994+Joshwa37@users.noreply.github.com>
2024-07-31 16:41:55 +0530

    Add files via upload

---
+++ read mode in python.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print('C:\some\name')
+print('C:\some\table')
+
+
+# In[4]:
+
+
+print(r'C:\some\name')
+print(r'C:\some\table')
+
+
+# In[ ]:
+
+
+
+
